472435f46d75cc82f5de393ad788d5152ebe926c
yash <yashvarshney2001@gmail.com>
2024-08-26 16:53:34 +0530

    socket url update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -52,7 +52,7 @@ function App() {
   const dispatch = useDispatch();
   useEffect(() => {
     if (user) {
-      const socketio = io("http://localhost:8000", {
+      const socketio = io("https://zero1-instagram-clone.onrender.com", {
         query: {
           userId: user?._id,
         },
